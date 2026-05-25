ecd7c4a53a196ec13942f918452734987c5c0c63
BtcBob <dobbscoin@gmail.com>
2026-05-25 23:46:11 +0000

    build(depends): disable qtprintsupport (windows print plugin fails to compile)

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -66,6 +66,7 @@ $(package)_config_opts += -silent
 $(package)_config_opts += -v
 $(package)_config_opts += -no-feature-printer
 $(package)_config_opts += -no-feature-printdialog
+$(package)_config_opts += -no-feature-printsupport
 
 ifneq ($(build_os),darwin)
 $(package)_config_opts_darwin = -xplatform macx-clang-linux
