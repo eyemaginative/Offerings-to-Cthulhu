d31db64ab0d42ba31bacc18b48ca0403ab2042e2
BtcBob <dobbscoin@gmail.com>
2026-05-28 13:45:01 +0000

    fix(qt): boost::placeholders in walletmodel.cpp

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -3,6 +3,12 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "walletmodel.h"
+#include <boost/version.hpp>
+#include <boost/bind/placeholders.hpp>
+#if BOOST_VERSION >= 106000
+using namespace boost::placeholders;
+#endif
+
 
 #include "addresstablemodel.h"
 #include "guiconstants.h"
