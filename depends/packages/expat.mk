48cccc0b555a80154d3d11e238248ba948d5f253
9019x <174914885+9019x@users.noreply.github.com>
2026-06-07 15:03:41 -0600

    depends: expat 2.1.0 -> 2.4.8 (static) This closes a large security gap. expat 2.1.0 (2012) is ~10 years and dozens of CVEs behind.
    - Flips from shared -> static

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,11 +1,18 @@
 package=expat
-$(package)_version=2.1.0
-$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_1_0/
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
+GCCFLAGS?=
+$(package)_version=2.4.8
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(package)_version))/
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=f79b8f904b749e3e0d20afeadecf8249c55b2e32d4ebb089ae378df479dcaf25
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+  $(package)_config_opts=--disable-shared --enable-static --without-docbook --without-xmlwf
+  $(package)_config_opts+=--libdir=$($($(package)_type)_prefix)/lib
+  $(package)_cflags+=-fPIC
+  $(package)_cxxflags_aarch64_linux = $(GCCFLAGS)
+  $(package)_cflags_aarch64_linux = $(GCCFLAGS)
+  $(package)_cxxflags_arm_linux = $(GCCFLAGS)
+  $(package)_cflags_arm_linux = $(GCCFLAGS)
 endef
 
 define $(package)_config_cmds
@@ -18,4 +25,8 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf share lib/cmake lib/*.la
 endef
