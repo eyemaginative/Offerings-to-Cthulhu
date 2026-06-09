8e7d78c5014c0e01bd354e59be3cddfe66790c22
9019x <174914885+9019x@users.noreply.github.com>
2026-06-08 19:11:15 -0600

    depends: dbus 1.8.6 -> 1.14.10 1.14.10 is the final release of the 1.14.x stable line. This is the last version that will compile with the `autotools` recipe. CVEs closed: - **CVE-2019-12749** - DBUS_COOKIE_SHA1 authentication bypass (local privilege   escalation via forged cookie file). Touches libdbus auth. Fixed 1.12.16.   CVSS 7.1 High. - **CVE-2020-12049** - File-descriptor leak in dbus-daemon - DoS. Fixed   in 1.12.18. CVSS 5.5 Medium. - **CVE-2022-42010 / 42011 / 42012** - message-parsing flaws (malformed messages   crash the consumer). Fixed 1.14.4. CVSS 6.5 Medium. - **CVE-2023-34969** - assertion-failure DoS. Fixed 1.14.8.   CVSS 6.5 Medium.
    The ones that are relevant here are **CVE-2022-42010 / 42011 /42012.**
    A malicious peer already on the user's session bus could send malformed messages
    to crash the GUI. Impact is limited to crashing our own process.

--- depends/packages/dbus.mk
+++ depends/packages/dbus.mk
@@ -1,12 +1,14 @@
 package=dbus
-$(package)_version=1.8.6
-$(package)_download_path=http://dbus.freedesktop.org/releases/dbus
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=eded83ca007b719f32761e60fd8b9ffd0f5796a4caf455b01b5a5ef740ebd23f
+$(package)_version=1.14.10
+$(package)_download_path=https://dbus.freedesktop.org/releases/dbus
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=ba1f21d2bd9d339da2d4aa8780c09df32fea87998b73da24f49ab9df1e36a50f
 $(package)_dependencies=expat
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-tests --disable-doxygen-docs --disable-xml-docs --disable-static --without-x
+  $(package)_config_opts += --disable-systemd --disable-launchd
+  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 endef
 
 define $(package)_config_cmds
